e08a5ab3c16dd52a04a0d046ea4d9a2d9e6a4682
Lorenzo Fiorentino <lorenzo.fiorentino@gmail.com>
2016-03-28 13:20:57 +0200

    up to 'Adding Controller and View'

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -2,13 +2,22 @@ class Racer
   include ActiveModel::Model
 
   #@id=doc[:_id].to_s
-  #:_id =>BSON::ObjectId.from_string(@id)
+  #:_id =>BSON::ObjectId(@id)
 
   attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
 
   #def to_s
   #  "#{@id}: #{@city}, #{@state}, pop=#{@population}"
   #end
+  def persisted?
+    !@id.nil?
+  end
+  def created_at
+    nil
+  end
+  def updated_at
+    nil
+  end
 
   # initialize from both a Mongo and Web hash
   def initialize(params={})
@@ -32,42 +41,89 @@ class Racer
   end
 
   def self.all(prototype={}, sort={:num=>1}, offset=0, limit=nil)
-      #map internal :population term to :pop document term
-      tmp = {} #hash needs to stay in stable order provided
-      sort.each {|k,v|
-        k = k.to_sym==:num ? :number : k.to_sym
-        tmp[k] = v  if [:first_name, :last_name, :number, :gender, :group, :secs].include?(k)
-      }
-      sort=tmp
+    #map internal :population term to :pop document term
+    tmp = {} #hash needs to stay in stable order provided
+    sort.each {|k,v|
+      k = k.to_sym==:num ? :number : k.to_sym
+      tmp[k] = v  if [:first_name, :last_name, :number, :gender, :group, :secs].include?(k)
+    }
+    sort=tmp
 
-      #convert to keys and then eliminate any properties not of interest
-      prototype=prototype.symbolize_keys.slice(:first_name, :last_name, :number, :gender, :group, :secs) if !prototype.nil?
+    #convert to keys and then eliminate any properties not of interest
+    prototype=prototype.symbolize_keys.slice(:_id, :first_name, :last_name, :number, :gender, :group, :secs) if !prototype.nil?
 
-      Rails.logger.debug {"getting all racers, prototype=#{prototype}, sort=#{sort}, offset=#{offset}, limit=#{limit}"}
+    Rails.logger.debug {"getting all racers, prototype=#{prototype}, sort=#{sort}, offset=#{offset}, limit=#{limit}"}
 
-      result=collection.find(prototype)
-            .projection({_id:true, first_name:true, last_name:true, number:true, gender:true, group:true, secs:true})
-            .sort(sort)
-            .skip(offset)
-      result=result.limit(limit) if !limit.nil?
+    result=collection.find(prototype)
+          .projection({_id:true, first_name:true, last_name:true, number:true, gender:true, group:true, secs:true})
+          .sort(sort)
+          .skip(offset)
+    result=result.limit(limit) if !limit.nil?
 
-      return result
-    end
+    return result
+  end
 
   # locate a specific document. Use initialize(hash) on the result to
   # get in class instance form
   def self.find id
     Rails.logger.debug {"getting racers #{id}"}
 
-    if id.is_a? String
-      @id = BSON::ObjectId(id)
-    else
-      @id = id
-    end
+    @id = BSON::ObjectId(id)
+
+    #if id.is_a? String
+    #  @id = BSON::ObjectId(id)
+    #else
+    #  @id = id
+    #end
 
     result=collection.find(:_id=>@id)
                   .projection({_id:true, first_name:true, last_name:true, number:true, gender:true, group:true, secs:true})
                   .first
     return result.nil? ? nil : Racer.new(result)
   end
+
+  def save
+    Rails.logger.debug {"saving #{self}"}
+
+    result = self.class.collection.insert_one(_id:@id, first_name:@first_name, last_name:@last_name, number:@number, gender:@gender, group:@group, secs:@secs)
+    @id=result.inserted_id
+  end
+
+  def update(params)
+    Rails.logger.debug {"updating #{self} with #{params}"}
+    #byebug
+
+    @number=params[:number].to_i
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @gender=params[:gender]
+    @group=params[:group]
+    @secs=params[:secs].to_i
+
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)# if !params.nil?
+
+    self.class.collection
+              .find(:_id => BSON::ObjectId.from_string(@id))
+              .update_one(
+                { :$set =>
+                  {
+                    number: @number,
+                    first_name: @first_name,
+                    last_name: @last_name,
+                    gender: @gender,
+                    group: @group,
+                    secs: @secs
+                  }
+                }
+              )
+  end
+
+  def destroy
+    Rails.logger.debug {"destroying #{self}"}
+
+    self.class.collection
+              .find(:number => @number)
+              .delete_one
+  end
+
 end
